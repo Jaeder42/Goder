06de941712c93718601396cfa1558cd028de3b3d
jaeder42 <jaeder42@gmail.com>
2020-10-06 08:51:49 +0200

    remote

---
+++ pages/blog/no-js-web.md
@@ -0,0 +1,4 @@
+# No JS
+## Can I build a website with no js?
+Short answer yes, you're here aren't you? But this just looks like html and css, I hear you telling me from the other side of your the screen. And sure, it is just that.
+The real meat of this comes from the path we are taking to end up with "just" html and css.  
--- server.go
+++ server.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"./controllers"
 )
 
 func main() {
+	t, err := template.ParseFiles("static/index.html")
 
-	http.HandleFunc("/", controllers.RenderHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", controllers.RenderHandlerHOF(t))
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
